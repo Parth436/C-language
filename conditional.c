5205a6030ee804559cba7b366a368fe0d444b778
Spartan <78797415+Parth436@users.noreply.github.com>
2024-10-13 23:08:57 +0530

    loops

--- conditional.c
+++ conditional.c
@@ -1,12 +1,58 @@
 #include<stdio.h>
-int main(){
-    int num;
-    scanf("%d",&num);
-    if(num>5){
-        printf("Greater");
+void main(){
+    //If else
+    // int num;
+    // scanf("%d",&num);
+    // if(num>5){
+    //     printf("Greater");
+    // }
+    // else{
+    //     printf("Smaller");
+    // }
+    //------------------------------------------------------------------
+    //Nested If Else
+    //if(num>10){
+    //if(num%15==0)
+    //}
+    //OR
+    //if((num>10)&&(num%15==0))
+    //-------------------------------------------------------------------
+
+    //If else ladder
+    //if(cgpa>10){
+    //printf("Invalid")
+    //}
+    //else if(cgpa==10){
+    //printf("A+")
+    //}else if(cgpa==9){
+    //printf("A")
+    //}and so on.....
+    //-------------------------------------------------------------------
+
+    //goto 
+    //int x=5;
+    //goto Label; 
+    //x=6;
+    //Label:printf("%d",x);
+    //----------------------------------------------------------------
+
+
+    //switch case
+    int cgpa;
+    scanf("%d",&cgpa);
+    switch (cgpa)
+    {
+    case 10:
+        printf("A+");
+        break;
+    case 9:
+        printf("A");
+        break;
+    
+    default:
+        printf("Invalid cgpa");
+        break;
     }
-    else{
-        printf("Smaller");
-    }
-    return 0;
 }
+
+
---
+++ loops.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+    //while loop
+    // int a=1;
+    // while(a<=5){
+    //     printf("%d\n",a);
+    //     a++;
+    // }
+    //-----------------------------------------------
+
+    //do while loop
+    // int a,i=1;
+    // scanf("%d",&a);
+    // do{
+    //     printf("%d\n",a*i);
+    //     i++;
+    // }while(i<=10);
+    //------------------------------------------------------
+
+    //For loop
+    for(int i=1;i<=5;i++){
+        printf("%d\n",i);
+    }
+}
