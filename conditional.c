98bd6cb45aa177cc0756b5c90c8d299acc67458b
Spartan <78797415+Parth436@users.noreply.github.com>
2024-10-13 22:22:39 +0530

    conditionals

---
+++ conditional.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>5){
+        printf("Greater");
+    }
+    else{
+        printf("Smaller");
+    }
+    return 0;
+}
